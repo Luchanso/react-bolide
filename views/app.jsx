be0ee947366a0f6a3429085d6e2553ca5bdc4713
Luchanso <luchansodev@gmail.com>
2017-04-22 15:17:06 +0300

    views: add logging assembly

--- views/app.jsx
+++ views/app.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Test from './components/Test';
+
+/* eslint-disable no-console */
+console.log(assembly);
+/* eslint-enable */
 
 ReactDOM.render(
   <Test text="Hello world" />,
