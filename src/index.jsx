57d6d973e6880cb3366881dbdda008465fa0e9b2
Luchanso <luchansodev@gmail.com>
2017-04-22 15:51:51 +0300

    view: rename views/app.jsx to src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Test from './components/Test';
+import Test from './views/components/Test';
 
 /* eslint-disable no-console */
 console.log({ assembly });
