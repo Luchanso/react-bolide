421048d582e59d9bcd4017abc0aaa71ae65ac083
Luchanso <luchansodev@gmail.com>
2017-04-22 15:47:20 +0300

    views: add react logo into views

--- views/components/Test/Test.jsx
+++ views/components/Test/Test.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './test.css';
+import reactLogo from '../../../assets/images/react-logo.svg';
 
 const Test = props => (
   <div className={styles.TestWrapper}>
     <h1 className={styles.Title}>{props.text}</h1>
+    <img alt="react-logo" src={reactLogo} />
   </div>
 );
 
